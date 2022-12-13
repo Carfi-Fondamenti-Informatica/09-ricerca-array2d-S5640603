789dda12c7fc4e394b415f72bbdd967aeab1d129
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-13 16:29:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,13 +5,14 @@ bool find(char lista_nomi[10][20], char target_nome[20], int &posizione){
     {
         for (int letter = 0; letter < 20; letter++)
         {
-            if (lista_nomi [nome][letter] != target_nome[letter])
-            {
-                return false;
+            if (lista_nomi [nome][letter] != target_nome[letter]){
+                letter = 20;  //20 variabile che vanno da (0-19)
+            }
+            if (letter == 19){ //se tutte le letteri sono uguali allora va bene
+                posizione = nome;//posizione, essendo che è nell'array va da 0-19
+                return true;
             }
         }
-        posizione = nome;
-        return true;
-        }
     }
-
+    return false;
+}
