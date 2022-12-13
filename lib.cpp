cbf1e50bf1c5d118e253d0c354edacac19521be5
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-13 11:22:02 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
+bool find(char lista_nomi[10][20], char target_nome[20], int &posizione){
+
+    for (int nome = 0; nome < 10; nome++)
+    {
+        for (int letter = 0; letter < 20; letter++)
+        {
+            if (lista_nomi [nome][letter] != target_nome[letter])
+            {
+                return false;
+            }
+        }
+        posizione = nome;
+        return true;
+        }
+    }
 
