ade49755a24f8699f0f07011ebcf7920e5b707fd
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-12-13 11:21:37 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
+#ifndef RICERCA_ARRAY_2D_ESERCITAZINE_9_LIB_H
+#define RICERCA_ARRAY_2D_ESERCITAZINE_9_LIB_H
+
+bool find(char lista_nomi[10][20], char target_nome[20], int &posizione);
+#endif //RICERCA_ARRAY_2D_ESERCITAZINE_9_LIB_H
 
